503cc2c69fcfd9668f5568682de40afa3f853602
EndLess728 <aryanmk.mk5@gmail.com>
2019-01-14 12:29:54 +0530

    Now address is seprated and we can also get custom marker location

--- GooglePlacePickerFinalSample/AppDelegate.swift
+++ GooglePlacePickerFinalSample/AppDelegate.swift
@@ -19,8 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        GMSPlacesClient.provideAPIKey("AIzaSyDN1NTmbMfaQVFhe3rmnHM4eNfZFIu4Faw")
         GMSServices.provideAPIKey("AIzaSyDN1NTmbMfaQVFhe3rmnHM4eNfZFIu4Faw")
+        GMSPlacesClient.provideAPIKey("AIzaSyDN1NTmbMfaQVFhe3rmnHM4eNfZFIu4Faw")
+
+
         
         return true
     }
--- GooglePlacePickerFinalSample/ViewController.swift
+++ GooglePlacePickerFinalSample/ViewController.swift
@@ -1,64 +1,55 @@
-//
-//  ViewController.swift
-//  GooglePlacePickerFinalSample
-//
-//  Created by MacMini on 1/9/19.
-//  Copyright © 2019 Immanent. All rights reserved.
-//
-
 import UIKit
 import GooglePlaces
 import GooglePlacePicker
 
 class ViewController: UIViewController,GMSPlacePickerViewControllerDelegate {
-
-    @IBOutlet weak var addressLabel: UILabel!
-    @IBOutlet weak var placeNameLabel: UILabel!
     
+
+    @IBOutlet weak var postalCodeLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var countryLabel: UILabel!
+    
+
+    var latlong:CLLocationCoordinate2D!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     // To receive the results from the place picker 'self' will need to conform to
     // GMSPlacePickerViewControllerDelegate and implement this code.
     func placePicker(_ viewController: GMSPlacePickerViewController, didPick place: GMSPlace) {
         // Dismiss the place picker, as it cannot dismiss itself.
         viewController.dismiss(animated: true, completion: nil)
+        print(place)
+        print(place.coordinate.latitude)
+        print(place.coordinate.longitude)
+        
+
+        
+        let location = CLLocation(latitude: place.coordinate.latitude, longitude: place.coordinate.longitude)
+        
+        
+        //This Function Will get all the address from marker locations
+        fetchCityAndCountry(from: location) { city, country, postalcode, error in
+            guard let city = city, let country = country , let postalcode = postalcode , error == nil else { return }
+            print(city + ", " + country + ", " +  postalcode) // Rio de Janeiro, Brazil , 18409
+            self.cityLabel.text = city
+            self.countryLabel.text = country
+            self.postalCodeLabel.text = postalcode
+            
+        }
+        
         
         print("Place name \(place.name)")
         print("PostalCode\(place.addressComponents)")
         print("Place address \(place.formattedAddress)")
         print("Place attributions \(place.attributions)")
         
-        //addressLabel.text = place.name
-        placeNameLabel.text = place.formattedAddress
-        
-        
-        //Get address Seperated like city,country,postalcode
-        let arrays : Array = place.addressComponents!
-        for i in 0..<arrays.count {
-            
-            let dics : GMSAddressComponent = arrays[i]
-            let str : String = dics.type
-            
-            if (str == "country") {
-                print("Country: \(dics.name)")
-            }
-            else if (str == "administrative_area_level_1") {
-                print("State: \(dics.name)")
-            }
-            else if (str == "administrative_area_level_2") {
-                print("City: \(dics.name)")
-            }
-            else if (str == "postal_code"){
-                print("PostalCode:\(dics.name)")
-                addressLabel.text = dics.name
-            }
-        }
+
     }
-        
+    
     
     
     func placePickerDidCancel(_ viewController: GMSPlacePickerViewController) {
@@ -68,8 +59,15 @@ class ViewController: UIViewController,GMSPlacePickerViewControllerDelegate {
         print("No place selected")
     }
     
-
-
+    func fetchCityAndCountry(from location: CLLocation, completion: @escaping (_ city: String?, _ country:  String?,_ postalCode: String?, _ error: Error?) -> ()) {
+        CLGeocoder().reverseGeocodeLocation(location) { placemarks, error in
+            completion(placemarks?.first?.locality,
+                       placemarks?.first?.country,
+                       placemarks?.first?.postalCode,
+                       error)
+        }
+    }
+    
     @IBAction func pickPlaceButton(_ sender: Any) {
         let config = GMSPlacePickerConfig(viewport: nil)
         let placePicker = GMSPlacePickerViewController(config: config)
@@ -82,5 +80,3 @@ class ViewController: UIViewController,GMSPlacePickerViewControllerDelegate {
         
     }
 }
-
-
